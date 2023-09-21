c65d2d69f06056eda4f7caad4218e651592337f9
Makobluey <Makobluey@gmail.com>
2023-09-21 11:42:30 -0400

    Dump Index.ts Fixed

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const app = new Application();
 app.use(async (context, next) => {
   try {
     await context.send({
-      root: ${Deno.cwd()}/public,
+      root: `${Deno.cwd()}/public`,
       index: "index.html",
     });
   } catch {
